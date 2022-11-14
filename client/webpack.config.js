5efb56eb8c63cd850eccab7c3b2368900e269d17
tonytran97 <tonytran_97@live.com>
2022-11-13 19:46:52 -0800

    + InjectManifest SW

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,7 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
@@ -22,7 +21,10 @@ module.exports = () => {
         template: './index.html',
         title: 'Text Editor',
       }),
-      new GenerateSW(),
+      new InjectManifest({
+        swSrc: './src/sw.js',
+        swDest: 'service-worker.js',
+      }),
       new WebpackPwaManifest({
         name: 'Text Editor',
         short_name: 'TextEditor',
